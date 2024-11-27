f7e8c4dfb91cc3e7094da108b195448187976689
Hi Prasanth...!!! <pintuprasanth9@gmail.com>
2024-11-27 21:47:55 +0530

    vercel debug db

--- services/dbService.js
+++ services/dbService.js
@@ -7,14 +7,9 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: "freedb_prasanthdb",
 });
-console.log('Running in:', process.env.NODE_ENV);
-console.log('Database Config:', {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-});
+
 
 const promisePool = pool.promise();
 
