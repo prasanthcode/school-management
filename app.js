6c8594d6c59f7190d36741ec8c5b04097da58df2
Hi Prasanth...!!! <pintuprasanth9@gmail.com>
2024-11-28 16:41:01 +0530

    vercel debug db

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', schoolRoutes);
 
+// Add the /hello route
+app.get('/hello', (req, res) => {
+  res.send('Hello! This is the School Management API.');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
 module.exports = app;
