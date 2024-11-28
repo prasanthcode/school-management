9ed363fb22458236962cbdc6b4d184c34bc372a2
Hi Prasanth...!!! <pintuprasanth9@gmail.com>
2024-11-28 16:11:29 +0530

    vercel debug db

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const schoolRoutes = require('./routes/schoolRoutes');
+const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', schoolRoutes);
 
--- services/dbService.js
+++ services/dbService.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "freedb_prasanthdb",
+  database: process.env.DB_NAME,
 });
 
 
